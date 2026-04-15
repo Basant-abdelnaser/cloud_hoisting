cfca8f4b25822752551d4f92160ba3859f0d9ced
Basant <basantabdelnaser438@gmail.com>
2026-04-15 06:39:10 +0200

    fix issue

--- app/components/comment/AddComment.tsx
+++ app/components/comment/AddComment.tsx
@@ -59,7 +59,7 @@ const AddComment = ({
     <div>
       <form onSubmit={handleSubmit}>
         <input
-          className={`border-2 border-gray-400 p-2 rounded-lg shadow-md  w-full mb-5 `}
+          className={`border-2 border-gray-400 p-2 rounded-lg shadow-md  w-full mb-5 focus:outline-none focus:ring-2 focus:ring-purple-500 `}
           type="text"
           placeholder="Add New Comment..."
           value={comment}
